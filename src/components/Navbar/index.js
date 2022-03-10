9204e43928f40aa7aed173e1351254c172ae29e0
subashdhanush <subashdhanush1618@gmail.com>
2022-03-10 20:57:55 +0530

    commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Nav,NavLink,NavIcon,Bars} from './NavbarElements';
+
+
+const Navbar = ({toggle}) => {
+  return (
+    <div>
+      <Nav>
+     <NavLink to='/'>Pizza</NavLink>
+     <NavIcon onClick={toggle}>
+     <p>Menu</p>
+     <Bars />  
+    </NavIcon> 
+      </Nav>
+    </div>
+  );
+};
+
+export default Navbar;
